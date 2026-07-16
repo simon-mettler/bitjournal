fe629e0c6a00193c0751a6b29e99a5dff90a8e89
Simon Mettler <contact@simonmettler.com>
2026-07-16 14:49:10 +0200

    add reka ui and lucide icons. update eslint and ts config

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -4,11 +4,6 @@ import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from 'eslint-config-prettier/flat'
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
-
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
@@ -19,6 +14,13 @@ export default defineConfigWithVueTs(
 
   ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
+
+  {
+    name: 'app/vue-rule-overrides',
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
 
   {
     ...pluginVitest.configs.recommended,
